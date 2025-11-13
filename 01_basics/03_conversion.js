53ab96e90ac5321b399ae695549007281c929cfe
Rakeshp05 <rakeshprajapati8450@gmail.com>
2025-11-13 12:32:07 +0000

    done

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,34 @@
+let age = null
+console.log(typeof(age))
+
+let valueInNumber = Number(age)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+
+/*
+- "33"=>33
+- "33abc=>NaN"
+- null=> 0
+- undefined => NaN
+-"Rakesh" =>NaN
+- true => 1 false =>0
+*/
+
+let changeboolean = ""
+let changeintoboolean =Boolean(changeboolean)
+console.log(changeintoboolean)
+
+// 1 => true 0 => false ""=> false "rakesh"=> true
+
+
+let anynumber = 54
+let Tostring = String(anynumber)
+console.log(typeof (Tostring))
+console.log(Tostring)
+
+
+/*-- OPERATIONS ---*/
+console.log(2+1)
+console.log("2"+1)
+console.log("2"+"1")
